64235f9014ca02cba320c7fadeadeac6d6aa6bb3
iramos5 <ismael.ramos@iesnervion.es>
2022-11-15 19:55:56 +0100

    nidea

---
+++ EjerciciosT3-IsmaelRamos/src/ejercicios/Ejercicio4.java
@@ -0,0 +1,23 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		/* Para dos números dados, a y b, es posible calcular el máximo común divisor (el número más grande que divide a ambos) 
+		 * mediante un algoritmo ineficiente pero sencillo: desde el menor de a y b, ir buscando, de forma decreciente (-1), 
+		 * el primer número que divide a ambos simultáneamente. Teniendo en cuenta lo que se acaba de explicar, 
+		 * realiza un programa que calcule el máximo común divisor de dos números.
+		 */
+		
+		Scanner scan = new Scanner (System.in);
+		int a, b, mcd;
+		
+		
+		// calcular maximo comun divisor
+		for (mcd--)
+
+	}
+
+}
