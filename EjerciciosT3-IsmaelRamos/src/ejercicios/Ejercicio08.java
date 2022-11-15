819ac39829535597c5ac330b59e59c868dcdbe2d
iramos5 <ismael.ramos@iesnervion.es>
2022-11-15 18:59:58 +0100

    8 enunciado

---
+++ EjerciciosT3-IsmaelRamos/src/ejercicios/Ejercicio08.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class Ejercicio08 {
+
+	public static void main(String[] args) {
+		/* Realiza un programa que lea y acepte únicamente aquellos que sean mayores que el último dado. 
+		 * La introducción de números finaliza con la introducción de un 0. Al final se mostrará:
+		 * El total de números introducidos, excluido el 0.
+		 * El total de números fallados.
+		 */
+		
+		
+
+
+	}
+
+}
