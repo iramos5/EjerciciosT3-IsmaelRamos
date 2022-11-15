5d3ee39adc0b459e337264c05a0ce977439ac74d
iramos5 <ismael.ramos@iesnervion.es>
2022-11-15 19:36:24 +0100

    Ejercicio 8 terminado. Falta comentar y pruebas

--- EjerciciosT3-IsmaelRamos/src/ejercicios/Ejercicio08.java
+++ EjerciciosT3-IsmaelRamos/src/ejercicios/Ejercicio08.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio08 {
 
 	public static void main(String[] args) {
@@ -9,7 +11,36 @@ public class Ejercicio08 {
 		 * El total de números fallados.
 		 */
 		
+		// variables
+		Scanner scan = new Scanner (System.in); // iniciar escaner
+		int inicial, num, fallos = 0, introducidos = 0; // numero inicial, numeros que se van introduciendo, total fallados y total introducidos
 		
+		// pedir datos
+		System.out.println("Introduce un número inicial: "); //pedir numero inicial
+		inicial = scan.nextInt(); // guardar numero inicial
+		
+		do {
+			System.out.println("Introduce un número: ");
+			num = scan.nextInt();
+			if (num>inicial && num!=0) {
+				introducidos++;
+			}
+			else if (num<=inicial && num!=0) {
+				System.out.println("El número es menor que el incial");
+				fallos++;
+				introducidos++;
+			}
+			inicial=num;
+			
+		} while (num!=0);
+		
+		introducidos= introducidos+1;
+		
+		System.out.println("Numero de fallos: "+fallos);
+		System.out.println("Cantidad de numeros introducidos: "+introducidos);
+		
+		// cerrar escaner
+		scan.close();
 
 
 	}
