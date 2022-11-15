d0ab8b41f44ddf2b9a1b9d8c2527114582cfa075
iramos5 <ismael.ramos@iesnervion.es>
2022-11-15 19:52:18 +0100

    nose

---
+++ EjerciciosT3-IsmaelRamos/src/ejercicios/Ejercicio6.java
@@ -0,0 +1,28 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		/*Realiza un programa que pida un número entero N entre 0 y 20 y 
+		 * luego muestre por pantalla los números desde 1 hasta N, uno en cada línea, 
+		 * repitiendo cada número tantas veces como su valor. Ejemplo:
+		 */
+		
+		// variables
+		Scanner scan = new Scanner (System.in);
+		int f=0, n, imprimir; //filas y numero
+		
+		
+		System.out.println("Introduce un numero del 0 al 20: ");
+		n = scan.nextInt();
+		
+		for (n=f; n<=f; n++) {
+			imprimir = n;
+			System.out.println(imprimir);
+		}
+		
+	}
+
+}
