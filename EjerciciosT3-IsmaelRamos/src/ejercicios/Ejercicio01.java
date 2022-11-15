2eee73b8747b28d56af73db2adcd6e3d8e21ede0
iramos5 <ismael.ramos@iesnervion.es>
2022-11-15 14:38:05 +0100

    otra vez el proyecto

---
+++ EjerciciosT3-IsmaelRamos/src/ejercicios/Ejercicio01.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		/* Escribe un programa que incremente la hora de un reloj. Se pedirán por teclado:
+		Hora
+		Minutos
+		Segundos
+		Cantidad de segundos a incrementar
+		La aplicación debe mostrar la nueva hora. Por ejemplo, si el usuario introduce hora=13, minutos=59 y segundos=51, y se desea incrementar en 10 segundos, el resultado a mostrar es 14:00:01.
+		*/
+
+	}
+
+}
